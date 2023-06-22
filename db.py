33fd7feeed104274f2718edaf76f4fb1de2c31e9
Grant Ramsay <seapagan@gmail.com>
2023-06-22 16:59:14 +0100

    add info on using sqlite3 instead of postgres
    Signed-off-by: Grant Ramsay <seapagan@gmail.com>

--- db.py
+++ db.py
@@ -3,6 +3,10 @@ from sqlalchemy.ext.asyncio import async_sessionmaker, create_async_engine
 from sqlalchemy.orm import declarative_base
 
 DATABASE_URL = "postgresql+asyncpg://postgres:postgres@localhost/postgres"
+# DATABASE_URL = "sqlite+aiosqlite:///./test.db"
+# Note that (as far as I can tell from the docs and searching) there is no need
+# to add 'check_same_thread=False' to the sqlite connection string, as
+# SQLAlchemy version 1.4+ will automatically add it for you when using SQLite.
 
 engine = create_async_engine(DATABASE_URL, echo=False)
 Base = declarative_base()
